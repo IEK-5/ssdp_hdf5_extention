783e58ca621f6a805a1b06f3bb6e8653f052fb34
Michael <m.gordon@fz-juelich.de>
2023-03-29 18:28:45 +0200

    fixup working on finishing out structs

--- examples/src/example_4_pretty.c
+++ examples/src/example_4_pretty.c
@@ -1,12 +1,14 @@
 #include <H5Epublic.h>
 #include <H5Fpublic.h>
+#include <H5Ipublic.h>
 #include <H5Ppublic.h>
+#include <H5Tpublic.h>
 #include <H5public.h>
 #include <hdf5.h>
 #include <hdf5_hl.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "arraylist.h"
+// #include "arraylist.h"
 
 # define ADDR_OF(X) ((int[]){X})
 
@@ -88,11 +90,36 @@ struct H5FileIOHandler{
     hid_t file_id;
     char* filename;
     IOMode mode;
-    arraylist* datasets;
 };
 
+struct H5DatasetHandler{
+    hid_t dataset_id;
+    hid_t dataspace_id;
+    int nrows;
+    int ncols;
+    double **data;
+    hid_t datatype;
+    char *name;
+    hid_t loc;
+};
 
-struct H5FileIOHandler* H5FileIOHandler_init(char* fn, IOMode mode, char ** datasets){
+struct H5DatasetHandler* H5DatasetHandler_init(double **data, int nrows, int ncols, hid_t h5_datatype, char *name, hid_t loc){
+    // init the dataspace
+    // init the dataset
+    // write the data
+    // close the dataset
+    // close the dataspace
+
+    // if close fails turn the pointer to NULL and free
+
+}
+
+void H5DatasetHandler_free(struct H5DatasetHandler *self){
+    // TODO
+}
+
+
+struct H5FileIOHandler* H5FileIOHandler_init(char* fn, IOMode mode){
     struct H5FileIOHandler* self;
     self = malloc(sizeof(*self));
     
@@ -112,43 +139,58 @@ struct H5FileIOHandler* H5FileIOHandler_init(char* fn, IOMode mode, char ** data
             self->file_id = H5Fopen(fn, H5F_ACC_RDONLY, H5P_DEFAULT);
             break;
         case WRITE:
-            // TODO
+            self->file_id = H5Fopen(fn, H5F_ACC_RDWR, H5P_DEFAULT);
+            break;
+        // always add default for savety
+        default:
+            self->file_id = H5I_INVALID_HID;
             break;
     }
+    
+    if (self->file_id == H5I_INVALID_HID){
+        goto efileio;
+    }   
 
-    efile:
-        arraylist_destroy(self->datasets);
-        free(self);
+    return self;
 
-    emalloc:
-        return NULL;
+efileio:
+    free(self);
+
+emalloc:
+    return NULL;
+
  };
 
-ErrorCode H5FileIOHandler_free(struct H5FileIOHandler *self){
-
+void H5FileIOHandler_free(struct H5FileIOHandler *self){
+    herr_t status = H5Fclose(self->file_id);
+    free(self);
 }
 
-ErrorCode H5FileIOHander_read_array(struct H5FileIOHandler *self, char *dataset_name, double **out_data, int *out_dims, int *out_rank){
+// ErrorCode H5FileIOHandler_discover_datasets(struct H5FileIOHandler *self){}
+
+ErrorCode H5FileIOHander_read_array(struct H5FileIOHandler *self, char *dataset_name, double **out_data, int nrows, int ncols, int out_rank){
+    // failure if dataset with dataset_name does not exist
+    struct H5DatasetHandler *dataset = H5DatasetHandler_init(out_data, nrows, ncols, H5T_IEEE_F64BE, dataset_name, self->file_id);
+    
 
 }
 
 ErrorCode H5FileIOHandler_write_array(struct H5FileIOHandler *self, char *dataset_name, double **in_data, int *in_dims, int rank){
-
+    // failure if drive is full
+    return FAILURE;
 }
 
-struct DatasetHandler{
-
-};
-
-void print_array(int *array, int length)
-{
-    for (int i = 0; i < length; i++) {  printf("%i ",array[i]);}
+void print_array(int *array, int length){
+    for (int i = 0; i < length; i++){
+        printf("%i ",array[i]);
+    }
     printf("\n");
 }
 
 // TODO check https://docs.hdfgroup.org/hdf5/v1_14/_h5_e__u_g.html#sec_error for error handling
 
 int main(){
+    /*
     int *arr = (int *) malloc(sizeof(int)*4);
     for(int i = 0; i<4; i++){
         arr[i]=i;
@@ -163,4 +205,6 @@ int main(){
     int* result = (int *) arraylist_pop(mylist);
     printf("%d\n",(int) *result);
     free(arr);
+    */
+    return 0;
 }
