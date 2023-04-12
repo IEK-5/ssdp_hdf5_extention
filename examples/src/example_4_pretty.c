131e82135062d671beb404b8968a7c3be6063f12
Michael <m.gordon@fz-juelich.de>
2023-04-12 19:17:38 +0200

    FIXUP finished implementing H5FileIoHandler methods and added a very basic smoke test as the main function. Next up: proper tests and implementation of float16

--- examples/src/example_4_pretty.c
+++ examples/src/example_4_pretty.c
@@ -3,6 +3,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <math.h>
+#include <time.h>
 
 # define ADDR_OF(X) ((int[]){X})
 
@@ -85,9 +86,23 @@ typedef enum {
 // Maybe use H5E_values ?
 typedef enum {
     SUCCESS = 0,
+    OUTOFMEMORY = 1,
     FAILURE = -1,
     // add others with different negative values
 } ErrorCode;
+
+const char* ErrorCode_to_string(ErrorCode err){
+    switch (err) {
+        case SUCCESS:
+            return "SUCCESS";
+        case OUTOFMEMORY:
+            return "OUTOFMEMORY";
+        case FAILURE:
+            return "GENERAL FAILURE";
+        default:
+            return "UNDEFINED ERROR VALUE";
+    }
+}
 
 struct H5FileIOHandler{
     hid_t file_id;
@@ -95,33 +110,45 @@ struct H5FileIOHandler{
     IOMode mode;
 };
 
-struct H5DatasetHandler{
-    /*
-        This struct helps managing a HD5Dataset which holds a n \times m array
-        The data is stored using 16-bit int to save diskspace => PRECISION MAY BE LOST
+/*
+    This struct helps managing a HD5Dataset which holds a n \times m array
+    The data is stored using 16-bit int to save diskspace => PRECISION MAY BE LOST
 
-        create it using int
-        write to file using write_array
-        read from file using read_array
-        when read read_X variables contain the data
-        
+    create it using int
+    write to file using write_array
+    read from file using read_array
+    when read read_X variables contain the data
+*/
+struct H5DatasetHandler{
+    /* 
+        TODO
+        checkout how to do 16 bit float and use it instead of uint16_t
+        float16 offers more flexibility e.g. also store coordinates in radians
+        make datatype and digits optional -> Enum?
+        main datatypes 16, 32 and 64 bit
     */
+    time_t t;
     hsize_t read_nrows;
     hsize_t read_ncols;
-    double **read_data;
+    double *read_data;
     hid_t datatype;
-    int digit_scale;
+    double digit_scale;
     char *name;
     hid_t loc;
 };
 
-struct H5DatasetHandler* H5DatasetHandler_init(char *name, hid_t loc){
-    /*
-        args:
-            name: name of dataset should be like a unix path e.g. /foo/bar/datasetname
-            loc: hid_t of the H5D file in which the dataset lives
-    */
-    struct H5DatasetHandler* self = malloc(sizeof(struct H5DatasetHandler));
+/*
+    Create and initialize a H5DDatasetHandler struct.
+    The struct must be freed!
+    args:
+        name: name of dataset should be like a unix path e.g. /foo/bar/datasetname
+        loc: hid_t of the H5D file in which the dataset lives
+    return:
+        pointer to H5DatasetHandler struct
+*/
+struct H5DatasetHandler* H5DatasetHandler_init(const char *name, hid_t loc){
+    struct H5DatasetHandler* self;
+    self = malloc(sizeof(*self));
     if (NULL == self){
         return NULL;
     }
@@ -135,13 +162,17 @@ struct H5DatasetHandler* H5DatasetHandler_init(char *name, hid_t loc){
     return self;
 }
 
-ErrorCode H5DatasetHandler_write_array(struct H5DatasetHandler *self, double** data, int nrows, int ncols){
-    /*
-        write a 2d c-array of doubles into a hdf5file
-    */
-
-    // write the 2d c-array data  into the HDF5 File using the paramaters in self
-
+/*
+    Write a continuous 1d array of doubles which is interpreted as a matrix into a HDF5 file using the H5DatasetHandler helper struct
+    args:
+        self: Handler object created by H5DatasetHandler_init
+        data: pointer to array of doubles
+        nrows: rows of matrix
+        ncols: columns of matrix
+    return:
+        SUCESS if operation worked otherwise an enum with a nonzero value
+*/ 
+ErrorCode H5DatasetHandler_write_array(struct H5DatasetHandler *self, double* data, int nrows, int ncols){
     /*
         TODOs:
             what do we do if a dataset already exists?
@@ -161,10 +192,10 @@ ErrorCode H5DatasetHandler_write_array(struct H5DatasetHandler *self, double** d
         H5Sclose(dataspace_id);
         return FAILURE;
     }
-    uint16_t **rounded_data = malloc(sizeof(uint16_t)*ncols*nrows);
-    for(int i = 0; i<ncols; i++){
-        for(int j = 0; j<nrows; j++){
-            rounded_data[i][j] = (uint16_t) round((data[i][j]*self->digit_scale));
+    uint16_t *rounded_data = malloc(sizeof(uint16_t)*ncols*nrows);
+    for(int i = 0; i<nrows; i++){
+        for(int j = 0; j<ncols; j++){
+            rounded_data[i*ncols+j] = (uint16_t) round((data[i*ncols+j]*self->digit_scale));
         }
     }
     status  = H5Dwrite(dataset_id, self->datatype, H5S_ALL, H5S_ALL, H5P_DEFAULT, rounded_data);
@@ -179,12 +210,20 @@ ErrorCode H5DatasetHandler_write_array(struct H5DatasetHandler *self, double** d
     }
 }
 
+/*
+    Read a 2D Dataset with fixed sizes from a HDF5 file into the Handler struct
+    This function allocated memory in H5DatasetHandler->read_data which needs to be freed!
+    args:
+        self: pointer returned by H5DatasetHandler_init
+    return:
+        SUCESS if operation worked otherwise an enum with a nonzero value
+*/
 ErrorCode H5DatasetHandler_read_array(struct H5DatasetHandler *self){
-    // read a 2d dataset from the file using the specifications in self
-
     /*
         todos
     */
+
+    ErrorCode return_val = SUCCESS;
     hid_t dataspace_id, dataset_id;
     herr_t status;
     hsize_t dims[2];
@@ -194,11 +233,13 @@ ErrorCode H5DatasetHandler_read_array(struct H5DatasetHandler *self){
 
     dataset_id = H5Dopen(self->loc, self->name, H5P_DEFAULT);
     if (H5I_INVALID_HID == dataset_id){
+        return_val = FAILURE;
         goto error;
     }
-    dataspace_id = H5Dget_space(dataspace_id);
+    dataspace_id = H5Dget_space(dataset_id);
     if(H5I_INVALID_HID == dataspace_id){
         H5Dclose(dataset_id);
+        return_val = FAILURE;
         goto error;
     }
    
@@ -207,6 +248,7 @@ ErrorCode H5DatasetHandler_read_array(struct H5DatasetHandler *self){
     if (n_dims < 0){
         H5Sclose(dataspace_id);
         H5Dclose(dataset_id);
+        return_val = FAILURE;
         goto error;
     }
 
@@ -219,17 +261,19 @@ ErrorCode H5DatasetHandler_read_array(struct H5DatasetHandler *self){
         printf("%s\n","Error when calling malloc");
         H5Sclose(dataspace_id);
         H5Dclose(dataset_id);
+        return_val = OUTOFMEMORY;
         goto error;
     }
 
     // todo create array out_data and fill from h5 file
-    status = H5Dread(dataset_id, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, buff);
+    status = H5Dread(dataset_id, self->datatype, H5S_ALL, H5S_ALL, H5P_DEFAULT, buff);
 
     if(status < 0){
         free(buff);
         printf("%s %d\n","Status code:", status);
         H5Sclose(dataspace_id);
         H5Dclose(dataset_id);
+        return_val = FAILURE;
         goto error;
     }
 
@@ -240,38 +284,53 @@ ErrorCode H5DatasetHandler_read_array(struct H5DatasetHandler *self){
         printf("%s\n","Error when calling malloc");
         H5Sclose(dataspace_id);
         H5Dclose(dataset_id);
+        return_val = OUTOFMEMORY;
         goto error;
     }
 
-    for(int i = 0; i<self->read_ncols; i++){
-        for (int j = 0; j<self->read_nrows; j++){
-            self->read_data[i][j] = buff[i*(self->read_ncols) + j]/10;
+    for(int i = 0; i<self->read_nrows; i++){
+        for (int j = 0; j<self->read_ncols; j++){
+            self->read_data[i*(self->read_ncols) + j] = ((double) buff[i*(self->read_ncols) + j])/self->digit_scale;
         }
     }
     free(buff);
     H5Sclose(dataspace_id);
     H5Dclose(dataset_id);
-    return SUCCESS;
+    return return_val;
 
 error:
     self->read_data = NULL;
     self->read_nrows = -1;
     self->read_ncols = -1;
-    return FAILURE;
+    return return_val;
 
 }
 
-void H5DatasetHandler_free(struct H5DatasetHandler **self_addr){
-    struct H5DatasetHandler *self = *self_addr;
-    if(NULL != self->read_data){
-        free(self->read_data);
+/*
+    we dont have this free because the memory is passed to the user and therfore we dont really need a free function anymore
+    void H5DatasetHandler_free(struct H5DatasetHandler **self_addr){
+        struct H5DatasetHandler *self = *self_addr;
+        if(NULL != self->read_data){
+            free(self->read_data);
+        }
+        free(self);
+        *self_addr = NULL;
     }
-    free(self);
-    *self_addr = NULL;
-}
+*/
 
+/*  
+    Create a H5FileIOHandler which makes it easier to open and create HDF5 files.
+    This functions allocates memory on the heap and opens resources. Free the struct using H5FileIOHandler_free!
 
+    args:
+        fn: name of the file to be opened or created
+        mode: wether to create a new file, open a file for reading or open a file for editing
+    return:
+        pointer to initialized H5FileIOHandler struct
+
+*/
 struct H5FileIOHandler* H5FileIOHandler_init(char* fn, IOMode mode){
+
     struct H5FileIOHandler* self;
     self = malloc(sizeof(*self));
     
@@ -283,6 +342,7 @@ struct H5FileIOHandler* H5FileIOHandler_init(char* fn, IOMode mode){
     self->filename = fn;
     
     herr_t status;
+    // todo change the mode to more ususually named words
     switch(mode){
         case CREATE:
             self->file_id = H5Fcreate(fn, H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
@@ -313,28 +373,53 @@ emalloc:
 
  };
 
+/*
+    Free the memory allocated and the resources opended by H5FileIOHandler_init
+    args:
+        self: pointer to H5FileIOHandler struct created by H5FileIOHandler_init
+*/
 void H5FileIOHandler_free(struct H5FileIOHandler *self){
+   
     herr_t status = H5Fclose(self->file_id);
     free(self);
 }
 
 // ErrorCode H5FileIOHandler_discover_datasets(struct H5FileIOHandler *self){}
 
-ErrorCode H5FileIOHander_read_array(struct H5FileIOHandler *self, char *dataset_name, double **out_data, int nrows, int ncols, int out_rank){
+/*
+    Read a 1D array of doubles which should be treated as a 2D Matrix from the dataset called dataset_name from the HDF5 file.
+    This function allocates memory for it's output which must be freed by the user!
+    args:
+        self: pointer created by H5FileIOHandler_init
+        dataset_name: name of the dataset in the HDF5 file
+        out_data: pointer output array value
+        out_nrows: number of rows of the Matrix
+        out_columns: number of columns of the Matrix
+    return:
+        SUCESS if it worked else a nonzero enum value
+ */
+ErrorCode H5FileIOHander_read_array(struct H5FileIOHandler *self, const char *dataset_name, double **out_data, int* out_nrows, int* out_ncols){
     // failure if dataset with dataset_name does not exist
     struct H5DatasetHandler *dataset = H5DatasetHandler_init(dataset_name, self->file_id);
     ErrorCode err;
     err = H5DatasetHandler_read_array(dataset);
-    if(FAILURE == err){
-
+    if(SUCCESS == err){
+        *out_data = dataset->read_data;
+        *out_nrows = dataset->read_nrows;
+        *out_ncols = dataset->read_ncols;
     }
-    
+    free(dataset);
+    return err;
 
 }
 
-ErrorCode H5FileIOHandler_write_array(struct H5FileIOHandler *self, char *dataset_name, double **in_data, int *in_dims, int rank){
+ErrorCode H5FileIOHandler_write_array(struct H5FileIOHandler *self, const char *dataset_name, double *data, int nrows, int ncols){
+    struct H5DatasetHandler *dataset = H5DatasetHandler_init(dataset_name, self->file_id);
+    ErrorCode err;
+    err = H5DatasetHandler_write_array( dataset, data,  nrows,  ncols);
+    free(dataset);
     // failure if drive is full
-    return FAILURE;
+    return err;
 }
 
 
@@ -342,21 +427,45 @@ ErrorCode H5FileIOHandler_write_array(struct H5FileIOHandler *self, char *datase
 // TODO check https://docs.hdfgroup.org/hdf5/v1_14/_h5_e__u_g.html#sec_error for error handling
 
 int main(){
-    /*
-    int *arr = (int *) malloc(sizeof(int)*4);
-    for(int i = 0; i<4; i++){
-        arr[i]=i;
+    int nrows = 5;
+    int ncols = 3;
+    int k = 0;
+    double *data = malloc(sizeof(double)*nrows*ncols);
+    const char *filename = "file4_pretty.h5";
+    const char *datasetname = "some_data";
+    double *read_data;
+    int read_nrows;
+    int read_ncols;
+    struct H5FileIOHandler* file;
+    ErrorCode err;
+
+    for(int i = 0; i<nrows; i++){
+        for(int j = 0; j<ncols; j++){
+            data[i*ncols+j]=(k++);
+        }
     }
-    print_array(arr, 4);
-    arr = (int *) realloc(arr, sizeof(&arr)*5);
-    arr[4] = 42;
-    print_array(arr, 5);
-    arraylist* mylist = arraylist_create();
-    arraylist_add(mylist, ADDR_OF(1));
-    arraylist_add(mylist, ADDR_OF(2));
-    int* result = (int *) arraylist_pop(mylist);
-    printf("%d\n",(int) *result);
-    free(arr);
-    */
+    
+    file = H5FileIOHandler_init(filename, CREATE);
+    err = H5FileIOHandler_write_array(file, datasetname, data, nrows, ncols);
+    printf("The writing of the data was: %s\n", ErrorCode_to_string(err));
+    H5FileIOHandler_free(file);
+    
+    file =  H5FileIOHandler_init(filename, READ);
+    err = H5FileIOHander_read_array(file, datasetname, &read_data, &read_nrows, &read_ncols);
+    printf("The reading of the data was: %s\n", ErrorCode_to_string(err));
+    H5FileIOHandler_free(file);
+
+    bool allsame = true;
+    const char * answer;
+    for(int i = 0; i<nrows; i++){
+        for(int j = 0; j<ncols; j++){
+            allsame &= data[i*ncols+j]==read_data[i*ncols+j];
+        }
+    }
+    
+    printf("Are the written and read data the same? %s \n", allsame ? "yes" : "no");
+    free(data);
+    free(read_data);
+    
     return 0;
 }
